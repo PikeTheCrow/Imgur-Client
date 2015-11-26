1cb2d657659fd4e74e6005b521364cb56136ad17
PikeTheCrow <pikethecrow@gmail.com>
2015-11-26 13:29:52 +0000

    added comments, image details and new scss

--- src/stores/image-store.jsx
+++ src/stores/image-store.jsx
@@ -9,10 +9,33 @@ module.exports = Reflux.createStore({
   getImages: function(topicId) {
     Api.get('topics/' + topicId).then(function(json) { //get makes a promise
       this.images = _.reject(json.data, function(image) { //evefr image in json.data, reject any image taht is an album
-        return image.is_album //from imgur
+        return image.is_album; //from imgur
       });
       this.triggerChange();
     }.bind(this)); //promise needs to be bond to this
+  },
+
+  getImage: function(id) {
+    Api.get('gallery/image/' + id).then(function(json){
+      if (this.images){
+        this.images.push(json.data);
+      } else {
+        this.images = [json.data];
+      }
+
+      this.triggerChange();
+    }.bind(this));
+  },
+
+  find: function(id) {
+    var image = _.findWhere(this.images, {id: id});
+
+    if (image) {
+      return image;
+    } else {
+      this.getImage(id);
+      return null;
+    }
   },
 
   triggerChange: function() {
